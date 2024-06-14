d812d30a14765c49b19b0efd87fee042580d00b7
Jody Klymak <jklymak@gmail.com>
2024-06-14 13:59:52 -0700

    New FFT

--- jmkxarray.py
+++ jmkxarray.py
@@ -7,8 +7,12 @@ _log = logging.getLogger(__name__)
 
 def gappy_interp(ds, *, dim='', xgrid=None, maxgap=np.Inf):
     """
+
+    OBSOLETE: use `ds.interpolate_na(maxgap=)
     interp ds on xgrid along dimension dim.  However don't
     interpolate across gaps larger than maxgap.
+
+
 
     Parameters
     ----------
@@ -59,7 +63,8 @@ def gappy_interp(ds, *, dim='', xgrid=None, maxgap=np.Inf):
     return ds.where(mask)
 
 
-def depth_to_iso(ds, pden='pden', depths='depths', xdim='along'):
+def depth_to_iso(ds, pden='pden', depths='depths', xdim='along',
+                 pden0=None, isodepths=None):
     """
     Map fields in this dataset to potential density coordinates.
 
@@ -77,19 +82,34 @@ def depth_to_iso(ds, pden='pden', depths='depths', xdim='along'):
 
     xdim : str (default 'along')
         Name of x coordinate of the data set
+
+    pden0 : array-like (default None)
+        Array of mean densities to interpolate onto.  If not provided, the
+        data set mean is used.
+
+    isodepths : array-like (default None)
+        Mean depths of the isopycnals defined in *pden0*.
     """
 
-    pden0 = ds[pden].mean(dim=xdim)
-    isodepths = ds[depths].where(pden0 > 0).dropna(dim=depths)
-    pden0 = pden0.dropna(dim=depths)
+    if pden0 is None:
+        pden0 = ds[pden].mean(dim=xdim)
+        isodepths = ds[depths].where(pden0 > 0).dropna(dim=depths)
+        pden0 = pden0.dropna(dim=depths)
 
     M = pden0.shape[0]
     N = ds[xdim].shape[0]
+    print(M, N)
 
-    dsiso = xr.Dataset(coords={'isodepths': isodepths, xdim: ds[xdim]})
+    dsiso = xr.Dataset(coords={'isodepths':('isodepths', isodepths.values),
+                               xdim: (xdim, ds[xdim].values)})
+    print(dsiso)
+    print(len(isodepths.values))
     for td in ds.variables:
-        _log.info('todo', td)
+        _log.info('todo', td, ds[td].shape)
+        print(td, ds[td].shape)
         if td == xdim:
+            pass
+        elif td == depths:
             pass
         elif ds[td].shape == ds[xdim].shape:
             dsiso[td] = (xdim, ds[td].values)
@@ -109,7 +129,7 @@ def depth_to_iso(ds, pden='pden', depths='depths', xdim='along'):
 def get_n_blocks(nfft, N, noverlap_min=None):
     if noverlap_min is None:
         noverlap_min = nfft / 2
-    for m in range(2, 500):
+    for m in range(2, 2000):
         noverlap = np.ceil(- (N - m * nfft) / (m - 1))
         if noverlap > 0 and noverlap >= noverlap_min:
             total = m * nfft - (m-1) * noverlap
@@ -128,8 +148,8 @@ def power_spec(da, nfft=256, xdim='along', ydim='depths', xunits='km',
     ----------
 
     da : DataArray
-        data array to calculate spectra over.  Spectra are calculated in the second dimension,
-        usually something like "x" or "time".
+        data array to calculate spectra over.  Spectra are calculated in the
+        second dimension, usually something like "x" or "time".
 
     nfft : integer
         length of FFT blocks
@@ -159,13 +179,12 @@ def power_spec(da, nfft=256, xdim='along', ydim='depths', xunits='km',
     """
     N = da[xdim].shape[0]
     window = np.hanning(nfft)
-    wnorm = (window * window).sum() / nfft
+    wnorm = (window * window).sum()
     noverlap, total, nblocks = get_n_blocks(nfft, N, noverlap_min=nfft/2)
     _log.info(noverlap, nblocks)
     dx = da[xdim].diff(dim=xdim).median().values
-    kx = np.arange(0, 1/2, 1/nfft) / dx
-
-    ps =  xr.Dataset(coords={'blocks': np.arange(nblocks), 'depths': da[ydim], 'kx': kx })
+    kx = np.arange(0, 1/2-1e-5, 1/nfft) / dx
+    ps =  xr.Dataset(coords={'blocks': np.arange(nblocks), 'depths': da[ydim].values, 'kx': kx })
     ps['kx'].attrs = {'description': 'wavenumber or frequency',
                       'units': f'cycles per {xunits}'}
     ps['spectrum'] = (('blocks', 'depths', 'kx'), np.zeros((nblocks, da[ydim].shape[0], len(kx))))
@@ -185,17 +204,62 @@ def power_spec(da, nfft=256, xdim='along', ydim='depths', xunits='km',
         xx = da.isel({xdim: slice(start, stop)})
         xx = xx - xx.mean(dim=xdim)
         ff = np.fft.fft(xx * window, axis=1)
-        pp = ff[:, :int(nfft/2)]*np.conj(ff[:, :int(nfft/2)]) * 2 / nfft / dx / wnorm
+        pp = ff[:, :int(nfft/2)]*np.conj(ff[:, :int(nfft/2)]) * 2 * dx / wnorm
         ps['spectrum'][nn, :, : ] = np.real(pp)
-        ps['along_start'][nn] = da[xdim].isel(along=start)
-        ps['along_stop'][nn] = da[xdim].isel(along=stop-1)
+        ps['along_start'][nn] = da[xdim].isel({xdim:start})
+        ps['along_stop'][nn] = da[xdim].isel({xdim: stop-1})
         start = stop - noverlap
     ps.attrs = {'nblocks': nblocks, 'noverlap': noverlap, 'nfft': nfft}
-
     return ps
 
 
+def multi_psd(da, minnfft=64, xdim='along', ydim='depths', xunits='km',
+              dataunits='kg/m^3'):
+
+    """
+    Calculate power spectra segments of a DataArray with different resolutions,
+    and return a spectra.  Interpolate over gaps that are a bit larger than the resolution
+    changes
+
+    nfft: size of fft blocks to try.
+        Maxgap for the current block will depend on the size of the next block.
+    """
+    N = len(da[xdim])
+    # minnfft = 64
+    nffts = [2*int(np.floor(N/3/2)*2)]
+    while np.floor(nffts[-1]/6) > minnfft:
+        nffts += [np.floor(nffts[-1]/6/2)*2]
+
+    # nffts = [2*int(np.floor(N/3)), 2*int(np.floor(N/18)), 2*int(np.floor(N/56))]
+
+    maxgaps = nffts[1:]
+    maxgaps += [2]
+    maxgaps = maxgaps[::-1]
+    # get the maxgap to interpolate over for each fft attempt
+    spec = []
+    kx = []
+    for nn, nfft in enumerate(nffts[::-1]):
+        dd = da.interpolate_na(dim=xdim, max_gap=maxgaps[nn])
+
+        p = power_spec(dd, nfft=int(nfft), xdim=xdim, ydim=ydim,
+                        xunits=xunits, dataunits=dataunits).mean(dim='blocks')
+        if nn == 0:
+            kmax = None
+            spec = p.spectrum.values[:, 1:]
+            kx = p.kx.values[1:]
+        else:
+            kmax = kx[0] - dkx / 2
+            spec = np.concatenate((p.spectrum.sel(kx=slice(None, kmax)).values[:, 1:], spec), axis=1)
+            kx = np.concatenate((p.kx.sel(kx=slice(None, kmax)).values[1:], kx))
+        dkx = p.kx.diff(dim='kx').median(dim='kx')
+    dout = xr.Dataset(coords={'depths': da[ydim].values, 'kx': kx })
+    dout['kx'] = ('kx', kx)
+    dout['spectrum'] = (('depths', 'kx'), spec)
+
+    return dout
 
 
+def whiten(kx, sp):
+    return sp*(np.pi*2*kx)**2
 
 
